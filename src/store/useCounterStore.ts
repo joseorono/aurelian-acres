b716cf2076df6e6546bf09ea04aaa46bd4085cd7
José Miguel Oroño <joseomaker@gmail.com>
2024-05-18 07:33:17 -0500

    Add Persist middleware to Store

--- src/store/useCounterStore.ts
+++ src/store/useCounterStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 
 const initialState: CounterState = {
   count: 0,
@@ -16,12 +16,39 @@ type CounterActions = {
   sum: (value: number) => void;
 };
 
-export const useCounterStore = create<CounterState & CounterActions>()(
-  devtools((set) => ({
-    count: 0,
-    increase: () => set((state) => ({ count: state.count + 1 })),
-    decrease: () => set((state) => ({ count: state.count - 1 })),
-    setTo: (value: number) => set(() => ({ count: value })),
-    sum: (value: number) => set((state) => ({ count: state.count + value })),
-  })),
+type CounterStateAndActions = CounterState & CounterActions;
+
+export const useCounterStore = create<CounterStateAndActions>()(
+  devtools(
+    persist(
+      (set) => ({
+        count: 0,
+        increase: () => set((state) => ({ count: state.count + 1 })),
+        decrease: () => set((state) => ({ count: state.count - 1 })),
+        setTo: (value: number) => set(() => ({ count: value })),
+        sum: (value: number) => set((state) => ({ count: state.count + value })),
+      }),
+      {
+        // Options
+        name: 'romeidle-counter-state',
+        version: 1, // a migration will be triggered if the version in the storage mismatches this one
+        onRehydrateStorage: (state: CounterStateAndActions) => {
+          console.log('Hydration starts');
+
+          // optional
+          return (state, error) => {
+            // First argument would be "state", but we don't need it
+            if (error) {
+              // Handle errors if needed
+              console.error('an error happened during hydration', error);
+              console.log('Failed state:', state);
+            } else {
+              // post-hydration logic, we could recompute some derived state here or trigger side-effects
+              console.log('hydration finished');
+            }
+          };
+        },
+      },
+    ),
+  ),
 );
