a498970091b54a550a023a958d4195ba9917a2dc
José Miguel Oroño <joseomaker@gmail.com>
2024-05-18 04:47:30 -0500

    Add flavor-text satirical headlines constant

---
+++ src/constants/flavor-text.ts
@@ -0,0 +1,39 @@
+// Just random, wacky flavor text for this game set in the Roman Empire.
+// I want it to read like a Monty Python skit or Satirical "Local News" Headlines about life in Ancient Rome.
+// I want them to be funny and short (a sentence or so). They have to be stuff that could plausibly happen in the setting or in Roman mythology.
+export const FLAVOR_TEXT_HEADLINES: string[] = [
+  '"Emperor is under Epicurean influence." Says Traitor.',
+  'Aqueduct repairs delayed again. Thirsty yet?',
+  'Jew in Rome claims to be the Son of God. Governor unimpressed.',
+  'Julius Caesar: "I came, I saw, I conquered."',
+  "Senator explains value of 'Bread and Circuses' to the masses. They were not amused.",
+  'Emperor declares war on the sea.',
+  'Judaea Local is Saturnalia King for a day. Declares "All debts are forgiven."',
+  'Legionnaire was found to be a secret Christian, fed to the lions.',
+  'Vestal Virgin: Is it hot here or is it just the flame?',
+  "Emperor's wife caught in a scandal involving a gladiator, takes unusual bath.",
+  '"vivere est bibere" claims Iberian scholar.',
+  "Cupid's arrows sold as prank gifts.",
+  "Olive shortage blamed on Zeus's snacks.",
+  'Olive shortage makes Philosopher rich.',
+  "Oracle predicts: 'Something big... eventually.'",
+  'Dunkey eats fig. Philosophers in Shambles.',
+  'Wine scandal: Bacchus demands stronger measures.',
+  'Emperor\'s new clothes: "They are fabulous."',
+  'Wait, they actually add lead to the wine?',
+  'Wait, they actually add resin to the wine?',
+  "Local bard arrested for 'Thou' overuse.",
+  "Local Farmer claims to have seen a 'flying chariot', crops ruined.",
+  "Area Man arrested for making 'too much sense'.",
+  'Area Man arrested for making gaulish jokes.',
+  'Area Man arrested for illegal garum production.',
+  '"Republican Ideals are just a phase." says Patrician.',
+  "Emperor's son fiddle practice disturbs neighbors.",
+  "Public Firefighters a 'waste of money' says Senator.",
+  "Public Firefighters a 'pipe dream' says Patrician.",
+  'Turkish Man claims to be descendant of Wolves.',
+  "'Gluteus do be Maximus' says Physician.",
+  "Galen's Morning Routine Questionable, says Killjoy'",
+];
+
+export const FAKE_HEADLINES_COUNT = FLAVOR_TEXT_HEADLINES.length;
