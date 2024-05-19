b71ece611cc85151d9df9ac1f20345d92a4895da
Carlos Sierra <carlosmatures@gmail.com>
2024-05-18 23:52:58 -0400

    pass sounds from soundFiles

--- src/components/demo/sounds-demo.tsx
+++ src/components/demo/sounds-demo.tsx
@@ -1,5 +1,5 @@
 import { Suspense, useEffect } from 'react';
-import { SoundsList, soundService } from '~/services/SoundService';
+import { SoundNames, soundService } from '~/services/SoundService';
 
 export default function SoundsDemo() {
   useEffect(() => {
@@ -13,13 +13,13 @@ export default function SoundsDemo() {
     <>
       <Suspense fallback={<SoundLoaderDemo />}>
         <h1>Sounds test</h1>
-        <button className="btn btn-primary" onClick={() => soundService.playSound(SoundsList.coinClick, 0.8, 0.7)}>
+        <button className="btn btn-primary" onClick={() => soundService.playSound(SoundNames.coinClick, 0.8, 0.7)}>
           play coin sound
         </button>
-        <button className="btn btn-primary" onClick={() => soundService.startMusic(SoundsList.backgroundMusic, 1)}>
+        <button className="btn btn-primary" onClick={() => soundService.startMusic(SoundNames.backgroundMusic, 1)}>
           play trademarked background music
         </button>
-        <button className="btn btn-primary" onClick={() => soundService.stopMusic(SoundsList.backgroundMusic)}>
+        <button className="btn btn-primary" onClick={() => soundService.stopMusic(SoundNames.backgroundMusic)}>
           stop trademarked background music
         </button>
       </Suspense>
