60a9df9f813ee652208a75dde0c22a4e91bbadee
José Miguel Oroño <joseomaker@gmail.com>
2024-05-18 23:16:21 -0500

    Modularize Store with the slices pattern

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,39 +1,29 @@
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
+import DEFAULT_VALUES from '~/constants/defaults';
 
-const initialState: CounterState = {
-  count: 0,
-};
+import { CounterSlice, CreateCounterSlice } from './slices/useCounterSlice';
 
-type CounterState = {
-  count: number;
-};
+//El type que se pasa a create es la union de todos los slices con &
+// Agregar aquí los slices que se creen en el proyecto
+export type GameStore = CounterSlice;
 
-type CounterActions = {
-  increase: () => void;
-  decrease: () => void;
-  setTo: (value: number) => void;
-  sum: (value: number) => void;
-};
-
-type CounterStateAndActions = CounterState & CounterActions;
-
-export const useCounterStore = create<CounterStateAndActions>()(
+// Esta es la store principal que contienen todos los slices
+// Esta es la Store principal que necesita tener el middleware, los slices no necesitan tener el middleware
+export const useStore = create<GameStore>()(
   devtools(
     persist(
-      (set) => ({
-        count: 0,
-        increase: () => set((state) => ({ count: state.count + 1 })),
-        decrease: () => set((state) => ({ count: state.count - 1 })),
-        setTo: (value: number) => set(() => ({ count: value })),
-        sum: (value: number) => set((state) => ({ count: state.count + value })),
+      (...set) => ({
+        // We spread ALL the slices here
+        ...CreateCounterSlice(...set), // This slice is just for testing, we could comment it out later
       }),
       {
         // Options
-        name: 'romeidle-counter-state',
+        name: 'romeidle-game-state',
         version: 1, // a migration will be triggered if the version in the storage mismatches this one
-        onRehydrateStorage: (state: CounterStateAndActions) => {
+        onRehydrateStorage: (state: GameStore) => {
           console.log('Hydration starts');
+          console.log('state: ', state);
 
           // optional
           return (state, error) => {
