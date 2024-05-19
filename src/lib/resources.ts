89a982dfd7f8f55cfa13872a7db8c3b5014a774e
José Miguel Oroño <joseomaker@gmail.com>
2024-05-19 03:23:37 -0500

    Complete and Test Resources lib functions

--- src/lib/resources.ts
+++ src/lib/resources.ts
@@ -1,15 +1,15 @@
-import BUILDINGS from '~/constants/buildings';
-import WORKERS from '~/constants/workers';
-import { Integer } from '~/types/number-types';
+import { BUILDINGS, BUILDINGS_ARRAY, BUILDINGS_COUNT } from '~/constants/buildings';
+import { WORKERS, WORKERS_ARRAY, WORKERS_COUNT } from '~/constants/workers';
+import { buildingData, playerResources, priceData, workersData } from '~/types/game-data-types';
 
 export function getBuildingById(id: number): buildingData | null {
   // Handle the case where the building is not found
-  return BUILDINGS.find((building) => building.id === id) ?? null;
+  return BUILDINGS_ARRAY.find((building: buildingData) => building.id === id) ?? null;
 }
 
 export function getWorkerById(id: number): workersData | null {
   // Handle the case where the worker is not found
-  return WORKERS.find((worker) => worker.id === id) ?? null;
+  return WORKERS_ARRAY.find((worker: workersData) => worker.id === id) ?? null;
 }
 
 export function getBuildingCost(id: number): priceData | null {
@@ -53,15 +53,48 @@ export function getBestAffordableBuilding(res: playerResources): number | null {
   }
 
   // Don't bother checking if the player can't afford the cheapest building
-  if (res.gold < BUILDINGS[0].costGold || res.grain < BUILDINGS[0].costGrain || res.stone < BUILDINGS[0].costStone) {
+  if (
+    res.gold < BUILDINGS_ARRAY[0].costGold ||
+    res.grain < BUILDINGS_ARRAY[0].costGrain ||
+    res.stone < BUILDINGS_ARRAY[0].costStone
+  ) {
     return null;
   }
 
   // Use for loop to go through the buildings array backwards and check if the player can afford the building
   // Return the first building that the player can afford
-  for (let i = BUILDINGS.length - 1; i >= 0; i--) {
-    if (canAffordBuilding(BUILDINGS[i].id, res)) {
-      return BUILDINGS[i].id;
+  for (let i = BUILDINGS_COUNT - 1; i >= 0; i--) {
+    if (canAffordBuilding(BUILDINGS_ARRAY[i].id, res)) {
+      return BUILDINGS_ARRAY[i].id;
+    }
+  }
+
+  // Return null if no building is affordable
+  return null;
+}
+
+export function getBestAffordableWorker(res: playerResources): number | null {
+  // This function assumes that the BUILDINGS array is sorted by profitability in ascending order
+
+  // Is the player broke???
+  if (res.gold == 0) {
+    return null;
+  }
+
+  // Don't bother checking if the player can't afford the cheapest building
+  if (
+    res.gold < WORKERS_ARRAY[0].costGold ||
+    res.grain < WORKERS_ARRAY[0].costGrain ||
+    res.stone < WORKERS_ARRAY[0].costStone
+  ) {
+    return null;
+  }
+
+  // Use for loop to go through the buildings array backwards and check if the player can afford the building
+  // Return the first building that the player can afford
+  for (let i = WORKERS_COUNT - 1; i >= 0; i--) {
+    if (canAffordWorker(WORKERS_ARRAY[i].id, res)) {
+      return WORKERS_ARRAY[i].id;
     }
   }
 
