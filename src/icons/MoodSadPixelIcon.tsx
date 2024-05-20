d70a4f19210192dbdbd669898bd20055d5ce8a39
José Miguel Oroño <joseomaker@gmail.com>
2024-05-19 19:07:55 -0500

    Add more Pixelart Icons

---
+++ src/icons/MoodSadPixelIcon.tsx
@@ -0,0 +1,19 @@
+// From https://pixelarticons.com/
+// https://github.com/halfmage/pixelarticons
+
+const MoonSadPixelIcon: React.FC<IPixelIconProps> = ({ size = 24, className = '', fill = '#fff' }) => {
+  return (
+    <svg
+      color={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      className={className}
+    >
+      <path d="M3 3h16v2H5v14h14v2H3V3zm18 0h-2v18h2V3zM11 15h2v2h-2v-2zm2-8h-2v6h2V7z" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default MoonSadPixelIcon;
---
+++ src/icons/WarningPixelIcon.tsx
@@ -0,0 +1,19 @@
+// From https://pixelarticons.com/
+// https://github.com/halfmage/pixelarticons
+
+const WarningPixelIcon: React.FC<IPixelIconProps> = ({ size = 24, className = '', fill = '#fff' }) => {
+  return (
+    <svg
+      color={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      className={className}
+    >
+      <path d="M20 2H2v20h2V4h16v12H6v2H4v2h2v-2h16V2h-2z" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default WarningPixelIcon;
