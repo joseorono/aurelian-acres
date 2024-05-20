1bd7ced80be123433b025cbf1d5221e0c2800360
Carlos Sierra <carlosmatures@gmail.com>
2024-05-19 23:10:35 -0400

    add getDisplayTileForKey
    Co-Authored-By: José Miguel Oroño <joseomaker@gmail.com>

--- src/components/town-display.tsx
+++ src/components/town-display.tsx
@@ -27,25 +27,42 @@ const spritesForTiles: Record<tilesKeys, string> = {
   priest: '',
 };
 
+function getDisplayTileForKey(key: Nullable<tilesKeys>) {
+  // If there's no key, simply return whatever tile we used by default or maybe nothing.
+  if (key == null) {
+    return null; // Can change later
+  }
+
+  return spritesForTiles[key];
+}
+
 function TownDisplay() {
   //  const gameStore = useStore();
 
   const renderableWorkerPerType = 6;
   const renderableBuildingPerType = 3;
-
+  const matrixHeight: number = 7;
+  const matrixWidth: number = 10;
   // ToDo: Test that this is Reactive. I really *hope* this updates reactively
   const workersCount: workerCount = useStore((state) => state.workerCount);
   const buildingsCount: buildingCount = useStore((state) => state.buildingCount);
 
   // Determine how many buildings and to render, limiting them to a certain number
   const buildingsToRender = auxObjectMap(buildingsCount, (count: number) => Math.min(count, renderableBuildingPerType));
-  const workersToRender = auxObjectMap(buildingsCount, (count: number) => Math.min(count, renderableWorkerPerType));
+  const workersToRender = auxObjectMap(workersCount, (count: number) => Math.min(count, renderableWorkerPerType));
 
   // ToDo: Generate a matrix of buildings and workers to render in random places, maybe
   // Or maybe start randomly but add a incrementally too it?? But that is more complex
   // Aqui estaria toda la logica interesante para generar el orden
   // Tenemos que generar un matrix donde todas las filas tenga el mismo numero de elementos.
-  const matrix: Array<Array<tilesKeys>> = [
+
+  // Generate empty Matrix with the right dimensions
+  let matrix: Array<Array<tilesKeys | null>> = Array(matrixHeight) // Generate the matrix
+    .fill(Array(matrixWidth).fill(null)); // Create the empty rows
+
+  // Populate the matrix with workers and building
+  // Este valor estatico loco borrarlo
+  matrix = [
     ['slave', 'agricola', 'fields', 'quarry', 'bakery', 'forum'],
     ['miner', 'baker', 'mercator', 'fields', 'quarry', 'bakery'],
     ['blacksmith', 'legionary', 'priest', 'fields', 'quarry', 'bakery'],
@@ -58,18 +75,10 @@ function TownDisplay() {
         {
           // Render the matrix
           matrix.map((row, rowIndex) => (
-            <div key={rowIndex} className="flex flex-row">
+            <div key={rowIndex} className="flex flex-row justify-items-start">
               {row.map((tileKey, tileIndex) => (
-                <div key={tileIndex} className="flex flex-col items-center">
-                  <div className="text-2xl">{spritesForTiles[tileKey]}</div>
-                  <div>
-                    {tileKey in workersToRender && workersToRender[tileKey] > 0 && (
-                      <div className="text-xs">{workersToRender[tileKey]}</div>
-                    )}
-                    {tileKey in buildingsToRender && buildingsToRender[tileKey] > 0 && (
-                      <div className="text-xs">{buildingsToRender[tileKey]}</div>
-                    )}
-                  </div>
+                <div key={tileIndex} className="flex flex-col items-center justify-items-center">
+                  <div className="townTile text-2xl">{getDisplayTileForKey(tileKey)}</div>
                 </div>
               ))}
             </div>
@@ -81,3 +90,4 @@ function TownDisplay() {
 }
 
 export default TownDisplay;
+
