c49a87044ed9307e07c3997c06b24bc4f765ab22
José Miguel Oroño <joseomaker@gmail.com>
2024-05-21 20:21:41 -0500

    Add missing properties and setter to event slice

--- src/store/slices/useEventSlice.ts
+++ src/store/slices/useEventSlice.ts
@@ -9,13 +9,14 @@ interface EventActions {}
 
 export type EventSlice = EventState & EventActions;
 
-export const CreateEventSlice: StateCreator<EventSlice> = () => ({
+export const CreateEventSlice: StateCreator<EventSlice> = (set) => ({
   visualModifiers: {
     rainingCoins: false,
     isNight: false,
     goldenOverlay: false,
     isFlood: false,
-    //bountifulHarvest: false, // raining wheat animation
+    bountifulHarvest: false,
   },
+  // Set Modifier by Key
+  setMultipliers: (values: clickerVisualModifiers) => set(() => ({ visualModifiers: values })),
 });
-
