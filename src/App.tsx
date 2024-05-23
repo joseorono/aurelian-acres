4d0ba25363f0b611d5689d4c9b0f4ec5c3ee2bf7
José Miguel Oroño <joseomaker@gmail.com>
2024-05-23 00:59:36 -0500

    Move all the test stuff to new DebugArea component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import CounterExample from '~/components/demo/counter-example';
-import StoreExample from '~/components/demo/store-demo';
-import ToasterDemo from '~/components/demo/toaster-demo';
-
 import { Toaster } from 'react-hot-toast';
 
 import '~/index.css';
-import AlertPixelIcon from '~/icons/AlertPixelIcon';
-import HeadlinesMarquee from '~/components/headlines-marquee';
-import SoundsDemo from './components/demo/sounds-demo';
-import LoopingProgressBar from './components/game/misc/loopingProgressBar';
+
 import CheckDoublePixelIcon from './icons/CheckDoubleIcon';
 import WarningPixelIcon from './icons/WarningPixelIcon';
 import LightBulbPixelIcon from './icons/LightBulbPixelIcon';
-import TownDisplay from './components/town-display';
-import GenericLoader from './components/loader/generic-loader';
-import DialogDemo from './components/demo/dialog-demo';
-import DialogDemo2 from './components/demo/dialog-demo-2';
+
 import { useEffect } from 'react';
-import MouseTracker from './components/effects/mouse-tracker';
+import MouseTracker from '~/components/effects/mouse-tracker';
 
 function App() {
   const toastClasses = 'pixel-rounded font-bold text-xl mx-2 w-11/12 md:w-fit min-w-[200px]';
@@ -38,32 +28,6 @@ function App() {
     <>
       <MouseTracker />
       <div id="app" vaul-drawer-wrapper="">
-        <div className="p-8">
-          <CounterExample />
-        </div>
-
-        <div className="p-8">
-          <StoreExample />
-        </div>
-        <div className="w-1/2">
-          <TownDisplay></TownDisplay>
-        </div>
-        {/* <SoundsDemo></SoundsDemo> */}
-
-        <div id="app">
-          <HeadlinesMarquee />
-          <div className="w-32">
-            <LoopingProgressBar durationInMs={1000} />
-          </div>
-          <ToasterDemo />
-          <AlertPixelIcon size={48} fill="white" />
-        </div>
-        <div className="p-4">
-          <DialogDemo />
-          <DialogDemo2 />
-          <SoundsDemo />
-        </div>
-        <GenericLoader />
         <Toaster
           gutter={12}
           toastOptions={{
---
+++ src/components/debug-area.tsx
@@ -0,0 +1,48 @@
+import TownDisplay from '~/components/town-display';
+import GenericLoader from '~/components/loader/generic-loader';
+import DialogDemo from '~/components/demo/dialog-demo';
+import DialogDemo2 from '~/components/demo/dialog-demo-2';
+
+import AlertPixelIcon from '~/icons/AlertPixelIcon';
+import HeadlinesMarquee from '~/components/headlines-marquee';
+import SoundsDemo from '~/components/demo/sounds-demo';
+import LoopingProgressBar from '~/components/game/misc/loopingProgressBar';
+
+
+import CounterExample from '~/components/demo/counter-example';
+import StoreExample from '~/components/demo/store-demo';
+import ToasterDemo from '~/components/demo/toaster-demo';
+
+
+export default function DebugArea() {
+  return (
+    <>
+      <div className="p-8">
+        <CounterExample />
+      </div>
+
+      <div className="p-8">
+        <StoreExample />
+      </div>
+      <div className="w-1/2">
+        <TownDisplay></TownDisplay>
+      </div>
+      {/* <SoundsDemo></SoundsDemo> */}
+
+      <div id="app">
+        <HeadlinesMarquee />
+        <div className="w-32">
+          <LoopingProgressBar durationInMs={1000} />
+        </div>
+        <ToasterDemo />
+        <AlertPixelIcon size={48} fill="white" />
+      </div>
+      <div className="p-4">
+        <DialogDemo />
+        <DialogDemo2 />
+        <SoundsDemo />
+      </div>
+      <GenericLoader />
+    </>
+  );
+}
