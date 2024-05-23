f6e06acae135d39d46d0b51cdd01afdcdc004e52
José Miguel Oroño <joseomaker@gmail.com>
2024-05-23 07:31:58 -0500

    Add demo code for Clicker and BackgroundWorker

--- src/components/game/background-worker.tsx
+++ src/components/game/background-worker.tsx
@@ -1,5 +1,7 @@
+import { useAtom, useAtomValue } from 'jotai';
 import { useEffect } from 'react';
 import { GAME_TICK_MS } from '~/constants/defaults';
+import { resourcesAtom } from '~/store/atoms';
 
 export default function BackgroundWorker() {
   // save the game tick as a state using useState
@@ -7,12 +9,30 @@ export default function BackgroundWorker() {
   // use the setInterval function to update the game tick every 1000ms
   // return the LoopingProgressBar component with the durationInMs prop set to 1000
 
+  const resources = useAtomValue(resourcesAtom);
+
   useEffect(() => {
+    console.log('BackgroundWorker mounted');
     const interval = setInterval(() => {
       // Update the game tick
+
+      // Update the value of resources in every DOM element with labelForGold, labelForGrain, and labelForStone
+      document.querySelectorAll('.labelForGold').forEach((el) => {
+        console.log('Found labelForGold');
+        el.textContent = resources.gold.toString();
+      });
+      document.querySelectorAll('.labelForGrain').forEach((el) => {
+        el.textContent = resources.grain.toString();
+      });
+      document.querySelectorAll('.labelForStone').forEach((el) => {
+        el.textContent = resources.stone.toString();
+      });
     }, GAME_TICK_MS);
 
-    return () => clearInterval(interval);
+    return () => {
+      console.log('BackgroundWorker unmounted');
+      clearInterval(interval);
+    };
   }, []);
 
   return <div className="hidden">{/* Useful Debug Info*/}</div>;
--- src/components/game/key/big-clicky-button.tsx
+++ src/components/game/key/big-clicky-button.tsx
@@ -1,23 +1,48 @@
 //This will be called like this
 // <BigClickyButton modifiers={visualModifiers} />
 
+import { useAtom, useAtomValue } from 'jotai';
+import { calculateActiveIncome } from '~/lib/resources';
+import { workersAtom, playerUpgradeAtom, playerLevelAtom, resourcesAtom } from '~/store/atoms';
 import { clickerVisualModifiers } from '~/types/game-data-types';
 
+import spriteClicker from '@/assets/star.png';
+
+function visualModifiersToClasses(modifiers: clickerVisualModifiers): string {
+  return `
+    big-clicky-button 
+    ${modifiers.rainingCoins ? 'raining-coins ' : ''}
+    ${modifiers.isNight ? 'is-night ' : ''}
+    ${modifiers.goldenOverlay ? 'golden-overlay ' : ''}
+    ${modifiers.isFlood ? 'is-flood ' : ''}
+    ${modifiers.bountifulHarvest ? 'bountiful-harvest ' : ''}
+  `;
+}
+
 export default function BigClickyButton(props: { modifiers: clickerVisualModifiers }) {
-  const { rainingCoins, isNight, goldenOverlay, isFlood, bountifulHarvest } = props.modifiers;
+  //const { rainingCoins, isNight, goldenOverlay, isFlood, bountifulHarvest } = props.modifiers;
+  const [resources, setResource] = useAtom(resourcesAtom);
+  const workers = useAtomValue(workersAtom);
+  const upgrade = useAtomValue(playerUpgradeAtom);
+  const playerLevel = useAtomValue(playerLevelAtom);
+
+  const activeIncome = calculateActiveIncome(workers, upgrade, playerLevel);
+  const addClasses = visualModifiersToClasses(props.modifiers);
 
   return (
-    <button
-      className={`
-            big-clicky-button
-            ${rainingCoins ? 'raining-coins' : ''}
-            ${isNight ? 'is-night' : ''}
-            ${goldenOverlay ? 'golden-overlay' : ''}
-            ${isFlood ? 'is-flood' : ''}
-            ${bountifulHarvest ? 'bountiful-harvest' : ''}
-        `}
-    >
-      Click Me
-    </button>
+    <img
+      src={spriteClicker}
+      id="big-clicky-button"
+      className={'mx-auto block w-full max-w-[75%] cursor-pointer' + addClasses}
+      onClick={() => {
+        setResource((resourcesDraft) => {
+          Object.assign(resourcesDraft, {
+            gold: resourcesDraft.gold + activeIncome.goldPerClick,
+            grain: resourcesDraft.grain + activeIncome.grainPerClick,
+            stone: resourcesDraft.stone + activeIncome.stonePerClick,
+          });
+        });
+      }}
+    />
   );
 }
