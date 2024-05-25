d9829b0635c8df59f0cd31972ea968dae4099c89
José Miguel Oroño <joseomaker@gmail.com>
2024-05-25 12:37:53 -0500

    Add small components to display basic info

---
+++ src/components/game/misc/town-name-view.tsx
@@ -0,0 +1,10 @@
+<h1 className="text-3xl">The Town</h1>;
+
+export default function TownNameView() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-800 p-4 text-white">
+      <h1 className="text-3xl">The Town</h1>
+      <p>Population: 100</p>
+    </div>
+  );
+}
--- src/components/game/panes/first-side-pane.tsx
+++ src/components/game/panes/first-side-pane.tsx
@@ -1,22 +1,21 @@
 import TownDisplay from '~/components/town-display';
 import GenericLoader from '../../loader/generic-loader';
 import { Suspense } from 'react';
-import { useAtomValue } from 'jotai';
-import { workersAtom } from '~/store/atoms';
-import { BASE_CITIZEN_COUNT } from '~/constants/defaults';
+
 import EventDisplay from '~/components/dialogs/event-display';
+import TownNameView from '../misc/town-name-view';
+import PopulationView from '../misc/population-view';
 
 interface IFirstSidePaneProps {
   className?: string;
 }
 
 const FirstSidePane: React.FC<IFirstSidePaneProps> = ({ className = '' }) => {
-  const workers = useAtomValue(workersAtom);
   return (
     <section className={className}>
       <div className="townName SectionHeader p-4 text-center text-white">
-        <h1 className="text-3xl">The Town</h1>
-        <p>Population: {Object.values(workers).reduce((a, b) => a + b, 0) + BASE_CITIZEN_COUNT}</p>
+        <TownNameView />
+        <PopulationView />
       </div>
       <div className="flex flex-auto flex-col items-center justify-center bg-gray-800">
         <EventDisplay />
