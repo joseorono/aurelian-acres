937f7cd293f86e3fd2ad06b1dddfd582a553cb48
José Miguel Oroño <joseomaker@gmail.com>
2024-05-25 13:48:48 -0500

    Some styles for AutoBuySection

--- src/components/game/key/auto-buy-section.tsx
+++ src/components/game/key/auto-buy-section.tsx
@@ -3,11 +3,11 @@ import LoopingProgressBar from '../misc/loopingProgressBar';
 export default function AutoBuySection() {
   return (
     <div className="flex bg-slate-800">
-      <div id="availableWorker" className="flex-auto bg-green-700 pt-4">
+      <div id="availableWorker" className="flex-auto bg-gradient-to-b from-red-600 to-red-900 p-4">
         Best Worker
       </div>
-      <div id="availableBuilding" className="flex flex-auto flex-col bg-blue-700 pt-4">
-        <div className="flex-auto">
+      <div id="availableBuilding" className="flex flex-auto flex-col bg-blue-800">
+        <div className="flex-auto bg-gradient-to-b from-blue-600 to-blue-800 p-4">
           <div>Best Building</div>
           <div>Building Cost</div>
         </div>
