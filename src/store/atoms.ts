27379ddc2ba3a39742b9ec0ddf25cd17f02034a9
Eduardo Angulo <eddangulom@gmail.com>
2024-05-25 03:58:29 -0400

    Remove commented code

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -67,36 +67,3 @@ export const visualModifiersAtom = withImmer(
 );
 
 export const eventsAtom = withImmer(atomWithStorage<eventData>('event', NOTHING_EVENT));
-// import { buildingKeys, buildingCount } from '~/types/game-data-types';
-// interface BuildingState {
-//   buildingCount: buildingCount;
-// }
-
-// interface BuildingActions {
-//   increaseBuilding: (key: buildingKeys, value: number) => void;
-//   decreaseBuilding: (key: buildingKeys, value: number) => void;
-// }
-
-// export type BuildingSlice = BuildingState & BuildingActions;
-
-// export const CreateBuildingSlice: StateCreator<BuildingSlice> = (set, get) => ({
-//   buildingCount: {
-//     fields: 0,
-//     quarry: 0,
-//     bakery: 0,
-//     forum: 0,
-//     smithy: 0,
-//     castra: 0,
-//     temple: 0,
-//   },
-//   increaseBuilding: (key: buildingKeys, value: number) =>
-//     set(() => ({ buildingCount: { ...get().buildingCount, [key]: get().buildingCount[key] + value } })),
-//   decreaseBuilding: (key: buildingKeys, value: number) =>
-//     set(() => ({
-//       buildingCount: {
-//         ...get().buildingCount,
-//         [key]: get().buildingCount[key] - value < 0 ? 0 : get().buildingCount[key] - value,
-//       },
-//     })),
-// });
-
