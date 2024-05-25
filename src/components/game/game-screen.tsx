1b5dc4bac351c57f1e15c48c74bb4b076f3b4eac
José Miguel Oroño <joseomaker@gmail.com>
2024-05-25 18:41:02 -0500

    Add placeholder navbar and styles

--- src/components/game/game-screen.tsx
+++ src/components/game/game-screen.tsx
@@ -1,14 +1,18 @@
 import MainGameArea from '~/components/game/panes/main-game-area';
 import FirstSidePane from '~/components/game/panes/first-side-pane';
 import SecondSidePane from './panes/second-side-pane';
+import GameNavbar from './game-navbar';
 
 export default function GameScreen() {
   return (
     <>
-      <div id="app" vaul-drawer-wrapper="" className="flex h-svh w-full">
-        <MainGameArea className="h-svh w-2/6 justify-items-start" />
-        <FirstSidePane className="flex h-svh w-2/6 flex-col justify-items-start" />
-        <SecondSidePane className="flex h-svh w-2/6 flex-col justify-items-start" />
+      <div id="app" vaul-drawer-wrapper="">
+        <GameNavbar />
+        <div id="game" className="h-game flex w-full">
+          <MainGameArea className="w-2/6 justify-items-start" />
+          <FirstSidePane className="flex w-2/6 flex-col justify-items-start" />
+          <SecondSidePane className="flex w-2/6 flex-col justify-items-start" />
+        </div>
       </div>
     </>
   );
