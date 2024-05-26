95ead6b771a34fbd2144bf6914e3f392fc287179
José Miguel Oroño <joseomaker@gmail.com>
2024-05-26 18:03:56 -0500

    Make resource indicators slightly smaller

--- src/components/game/misc/resource-indicator.tsx
+++ src/components/game/misc/resource-indicator.tsx
@@ -4,7 +4,7 @@ export default function ResourceIndicator() {
   return (
     <div className="bg-blue-noise p-2 py-3 text-center text-2xl">
       Current resources:
-      <div className="text-center text-5xl">
+      <div className="text-center text-2xl">
         {' '}
         <div className="mx-auto mt-2 flex w-[400px] flex-row justify-center gap-2 text-center text-2xl">
           <div className="pixel-rounded resource-display flex flex-auto p-2 px-3 pt-4">
