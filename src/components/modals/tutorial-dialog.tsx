57a766b0d31073a141ebbaf88762d6e218e29ae9
Carlos Sierra <carlosmatures@gmail.com>
2024-05-26 18:10:46 -0400

    rough draft of the tutorial text

--- src/components/modals/tutorial-dialog.tsx
+++ src/components/modals/tutorial-dialog.tsx
@@ -4,11 +4,11 @@ export default function TutorialDialog() {
       <h1>Welcome to Aurelian Acres!</h1>
       <p>
         You are a legate sent by the one an only Caesar! your mission is to lift a romanian civilization in this strange
-        land the way you were taught how. The roman way.
+        land the way you were taught how. The <i>roman</i> way.
       </p>
-      <p>Click on the big coin to win resources.</p>
-      <p>Buy.</p>
-      <p>Click on the big coin to win resources.</p>
+      <p>Click on the big coin to gain resources.</p>
+      <p>Buy workers to increase resources per click</p>
+      <p>Buy buildings to increase resources per second</p>
     </>
   );
 }
