86c75c92f666cee06db7e2e0aac2ddb4988456ea
Carlos Sierra <carlosmatures@gmail.com>
2024-05-26 19:53:24 -0400

    updated images

--- src/components/game/misc/resource-indicator.tsx
+++ src/components/game/misc/resource-indicator.tsx
@@ -1,4 +1,7 @@
 import '~/css/resource-display.css';
+import coin from '@/assets/resources/coin.png';
+import stone from '@/assets/resources/stone.png';
+import wheat from '@/assets/resources/wheat.png';
 
 export default function ResourceIndicator() {
   return (
@@ -8,13 +11,22 @@ export default function ResourceIndicator() {
         {' '}
         <div className="mx-auto mt-2 flex w-full max-w-[400px] flex-row flex-wrap justify-center gap-2 text-center text-2xl">
           <div className="pixel-rounded resource-display flex  min-w-[130px] max-w-full flex-auto p-2 px-3 pt-4">
-            <span>🪙</span>: <span className="labelForGold flex-auto text-right">0</span>
+            <span>
+              <img src={coin} alt="coin" width={32} height={32} />
+            </span>
+            <span className="labelForGold flex-auto text-right">0</span>
           </div>
           <div className="pixel-rounded resource-display flex  min-w-[130px] max-w-full flex-auto p-2 px-3 pt-4">
-            <span>🌾</span> : <span className="labelForGrain flex-auto text-right">0</span>
+            <span>
+              <img src={stone} alt="stone" width={32} height={32} />
+            </span>
+            <span className="labelForGrain flex-auto text-right">0</span>
           </div>
           <div className="pixel-rounded resource-display flex  max-w-full flex-auto p-2 px-3 pt-4">
-            <span>🪨</span>: <span className="labelForStone flex-auto text-right">0</span>
+            <span>
+              <img src={wheat} alt="wheat" width={32} height={32} />
+            </span>
+            <span className="labelForStone flex-auto text-right">0</span>
           </div>
         </div>
       </div>
