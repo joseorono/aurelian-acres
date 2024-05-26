66738d387c0dd31fd7d388441556a8777f8e71b3
José Miguel Oroño <joseomaker@gmail.com>
2024-05-26 18:19:53 -0500

    Add Responsive behavior for Resource Indicator

--- src/components/game/misc/resource-indicator.tsx
+++ src/components/game/misc/resource-indicator.tsx
@@ -6,14 +6,14 @@ export default function ResourceIndicator() {
       Current resources:
       <div className="text-center text-2xl">
         {' '}
-        <div className="mx-auto mt-2 flex w-[400px] flex-row justify-center gap-2 text-center text-2xl">
-          <div className="pixel-rounded resource-display flex flex-auto p-2 px-3 pt-4">
+        <div className="mx-auto mt-2 flex w-full max-w-[400px] flex-row flex-wrap justify-center gap-2 text-center text-2xl">
+          <div className="pixel-rounded resource-display flex  min-w-[130px] max-w-full flex-auto p-2 px-3 pt-4">
             <span>🪙</span>: <span className="labelForGold flex-auto text-right">0</span>
           </div>
-          <div className="pixel-rounded resource-display flex flex-auto p-2 px-3 pt-4">
+          <div className="pixel-rounded resource-display flex  min-w-[130px] max-w-full flex-auto p-2 px-3 pt-4">
             <span>🌾</span> : <span className="labelForGrain flex-auto text-right">0</span>
           </div>
-          <div className="pixel-rounded resource-display flex flex-auto p-2 px-3 pt-4">
+          <div className="pixel-rounded resource-display flex  max-w-full flex-auto p-2 px-3 pt-4">
             <span>🪨</span>: <span className="labelForStone flex-auto text-right">0</span>
           </div>
         </div>
