c83669782328bd61a1c58a253203c1ab24414e19
Eduardo Angulo <eddangulom@gmail.com>
2024-05-26 21:03:32 -0400

    Remove relative reference

--- src/icons/resourceIcons.tsx
+++ src/icons/resourceIcons.tsx
@@ -1,6 +1,6 @@
-import coin from '@/assets/resources/coin.png';
-import stone from '@/assets/resources/stone.png';
-import wheat from '@/assets/resources/wheat.png';
+import coin from '/assets/resources/coin.png';
+import stone from '/assets/resources/stone.png';
+import wheat from '/assets/resources/wheat.png';
 
 export function Coin({ size = 24, className = '' }) {
   return <img src={coin} alt="coin" width={size} height={size} className={className} />;
