df2dcf9ac7e848a8d23d1a2996b65706963a48d4
José Miguel Oroño <joseomaker@gmail.com>
2024-05-27 03:41:06 -0500

    Tweak constants to make town denser

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -60,16 +60,15 @@ const DEFAULT_VALUES: gameStateSnapshot = {
   progressToggles: {},
 } as const;
 
-export const DEFAULT_TOWNDISPLAY_ROWS = 20;
-export const DEFAULT_TOWNDISPLAY_COLUMNS = 40;
+export const DEFAULT_TOWNDISPLAY_ROWS = 17;
+export const DEFAULT_TOWNDISPLAY_COLUMNS = 17;
 export const CONST_MAX_BUILDING_TYPE = 20;
 // Used for probability calculations. Must be float between 0 and 1.
 // super low bc we dont have many buildings in the tests
-export const TOWN_DENSITY_BIAS = 0.05;
+export const TOWN_DENSITY_BIAS = 0.4;
 export const GAME_TICK_SECONDS: number = 1; // We could change this for performance/stress testing later
 export const GAME_TICK_MS: number = GAME_TICK_SECONDS * 1000; // We could change this for performance/stress testing later
 // For show and variation. Just a random number of citizens to start with and vary
 export const BASE_CITIZEN_COUNT: number = randIntInRange(21, 29);
 
 export default DEFAULT_VALUES;
-
