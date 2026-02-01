ecbb3ab9efd3e7ad7c6edd3f8a06c1d57482693d
José Miguel Oroño <joseomaker@gmail.com>
2026-02-01 18:56:01 -0500

    Swap default and order of shop tabs
    The default tab is now 'buildings' instead of 'workers', and the order of the tabs has been switched to display 'Buildings' first. Logic and tab switching have been updated accordingly to reflect this change.

--- src/components/game/panes/second-side-pane.tsx
+++ src/components/game/panes/second-side-pane.tsx
@@ -4,13 +4,13 @@ import WorkersShop from '../shop/workers-shop';
 import { SoundNames, soundService } from '~/services/sound-service';
 import PlayerLevelUpgrades from '../key/player-level-upgrades';
 
-const VALID_SHOP_TABS = ['workers', 'buildings'] as const;
+const VALID_SHOP_TABS = ['buildings', 'workers'] as const;
 type ShopTab = (typeof VALID_SHOP_TABS)[number];
 
 export default function SecondSidePane({ className = '' }) {
-  const [currentTab, setTab] = useState<ShopTab>('workers');
+  const [currentTab, setTab] = useState<ShopTab>('buildings');
 
-  const isWorkersTab = currentTab === 'workers';
+  const isBuildingsTab = currentTab === 'buildings';
 
   const handleTabChange = (tab: ShopTab) => {
     soundService.playSound(SoundNames.click, 0.2, 0.1);
@@ -28,23 +28,23 @@ export default function SecondSidePane({ className = '' }) {
         <div role="tablist" className="tabs tabs-bordered sticky top-0 z-50 mb-2 bg-[#3e4f59] pt-2">
           <a
             role="tab"
-            onMouseDown={() => handleTabChange('workers')}
-            className={'tab h-auto !p-1 !text-xl leading-none ' + (isWorkersTab ? 'tab-active font-bold' : '')}
-          >
-            Workers
-          </a>
-          <a
-            role="tab"
             onMouseDown={() => handleTabChange('buildings')}
-            className={'tab h-auto !p-1 !text-xl leading-none  ' + (isWorkersTab ? '' : 'tab-active font-bold')}
+            className={'tab h-auto !p-1 !text-xl leading-none ' + (isBuildingsTab ? 'tab-active font-bold' : '')}
           >
             Buildings
           </a>
+          <a
+            role="tab"
+            onMouseDown={() => handleTabChange('workers')}
+            className={'tab h-auto !p-1 !text-xl leading-none  ' + (isBuildingsTab ? '' : 'tab-active font-bold')}
+          >
+            Workers
+          </a>
         </div>
-        <div className={`${isWorkersTab ? '!hidden' : ''}`}>
+        <div className={isBuildingsTab ? '' : '!hidden'}>
           <BuildingsShop />
         </div>
-        <div className={isWorkersTab ? '' : '!hidden'}>
+        <div className={isBuildingsTab ? '!hidden' : ''}>
           <WorkersShop />
         </div>
       </div>
